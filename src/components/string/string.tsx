46a18f0eefca910ea9785707c22baf86a5aec98d
Alexey Krutyakov <coder.arctik@gmail.com>
2023-12-10 15:11:56 +0300

    feat: add circles in SrtingComponent

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,22 +1,37 @@
 import styles from './string.module.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import { Input } from '../ui/input/input';
 import { Button } from '../ui/button/button';
+import { ElementStates } from '../../types/element-states';
+import { Circle, CircleProps } from '../ui/circle/circle';
 
 export const StringComponent: React.FC = () => {
-  const [word, setWord] = useState('');
-
-  // const { values, handleChange } = useForm({
-  //   word: '',
-  // });
+  const [circles, setCircles] = useState<CircleProps[]>([]);
 
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const str = document.querySelector('input')?.value;
-    str && setWord(str);
-    console.log(str);
+
+    const word = document.querySelector('input')?.value;
+    let letters: string[] = [];
+
+    if (word) letters = Array.from(word);
+
+    console.log(letters);
+
+    letters.map((letter, index) =>
+      setCircles(circles => [
+        ...circles,
+        {
+          letter,
+          index,
+          state: ElementStates.Default,
+        },
+      ])
+    );
   };
+
+  useEffect(() => console.log('circles', circles), [circles]);
 
   return (
     <SolutionLayout title="Строка">
@@ -33,6 +48,19 @@ export const StringComponent: React.FC = () => {
         />
         <Button text="Развернуть" type="submit" />
       </form>
+      {circles.length > 0 && (
+        <div className={styles.scheme}>
+          {circles.map(circle => {
+            return (
+              <Circle
+                letter={circle.letter}
+                key={circle.index}
+                state={circle.state}
+              />
+            );
+          })}
+        </div>
+      )}
     </SolutionLayout>
   );
 };
