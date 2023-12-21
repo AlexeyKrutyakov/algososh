7129ce55140b164f536aa0ec1754294d39986f69
Alexey Krutyakov <coder.arctik@gmail.com>
2023-12-21 22:38:33 +0300

    feat: add initialElements for list

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,13 +1,41 @@
 import styles from './list-page.module.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import { Input } from '../ui/input/input';
 import { Button } from '../ui/button/button';
-import { Circle } from '../ui/circle/circle';
+import { Circle, CircleProps } from '../ui/circle/circle';
 import { ArrowIcon } from '../ui/icons/arrow-icon';
 import { ElementStates } from '../../types/element-states';
+import { HEAD, TAIL } from '../../constants/element-captions';
 
 export const ListPage: React.FC = () => {
+  const [elements, setElements] = useState<CircleProps[]>([]);
+  const initialElements = [
+    {
+      index: 0,
+      head: HEAD,
+      letter: '0',
+    },
+    {
+      index: 1,
+      letter: '34',
+    },
+    {
+      index: 2,
+      letter: '8',
+    },
+    {
+      index: 3,
+      letter: '8',
+      tail: TAIL,
+    },
+  ];
+
+  useEffect(() => {
+    setElements([...initialElements]);
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <SolutionLayout title="Связный список">
       <form name="controls_value" className={styles.controls_value}>
@@ -33,27 +61,20 @@ export const ListPage: React.FC = () => {
         <Button extraClass={styles.button_bot} text="Удалить по индексу" />
       </form>
       <div className={styles.scheme}>
-        <Circle head={'head'} index={0} letter="0" />
-        <ArrowIcon />
-        <Circle
-          index={1}
-          head={
-            <Circle letter="85" state={ElementStates.Changing} isSmall={true} />
-          }
-          letter="34"
-        />
-        <ArrowIcon />
-        <Circle index={2} letter="8" />
-        <ArrowIcon />
-        <Circle index={2} letter="8" />
-        <ArrowIcon />
-        <Circle index={2} letter="8" />
-        <ArrowIcon />
-        <Circle index={2} letter="8" />
-        <ArrowIcon />
-        <Circle index={2} letter="8" />
-        <ArrowIcon />
-        <Circle index={3} letter="1" tail={'tail'} />
+        {elements.map((element, index) => {
+          return (
+            <article key={index} className={styles.article}>
+              <Circle
+                index={element.index}
+                letter={element.letter}
+                head={element.head}
+                tail={element.tail}
+                state={element.state}
+              />
+              {index !== elements.length - 1 && <ArrowIcon />}
+            </article>
+          );
+        })}
       </div>
     </SolutionLayout>
   );
