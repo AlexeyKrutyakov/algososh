f93475fd98bdda0e1d9eabfb59b7620fee61a0a2
Alexey Krutyakov <coder.arctik@gmail.com>
2023-12-25 18:03:20 +0300

    fix: disable controls when function works and add value to input element

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,5 +1,5 @@
 import styles from './string.module.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import { Input } from '../ui/input/input';
 import { Button } from '../ui/button/button';
@@ -7,16 +7,31 @@ import { Circle, CircleProps } from '../ui/circle/circle';
 import turnOverCircles from '../../utils/turn-over-circles';
 
 export const StringComponent: React.FC = () => {
-  const [str, setStr] = useState('');
+  const [str, setStr] = useState<string>('');
   const [circles, setCircles] = useState<CircleProps[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
+  const [isInputDisabled, setIsInputDisabled] = useState<boolean>(false);
 
   const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setIsLoading(true);
+    setIsButtonDisabled(true);
+    setIsInputDisabled(true);
     await turnOverCircles(str, setCircles);
+    setIsButtonDisabled(false);
+    setIsInputDisabled(false);
+    setStr('');
     setIsLoading(false);
   };
+
+  useEffect(() => {
+    if (str === '') {
+      setIsButtonDisabled(true);
+    } else {
+      setIsButtonDisabled(false);
+    }
+  }, [str]);
 
   return (
     <SolutionLayout title="Строка">
@@ -30,9 +45,11 @@ export const StringComponent: React.FC = () => {
           maxLength={11}
           isLimitText={true}
           extraClass={styles.input}
+          value={str}
           onChange={event => setStr(event.currentTarget.value)}
+          disabled={isInputDisabled}
         />
-        <Button isLoader={isLoading} text="Развернуть" type="submit" />
+        <Button isLoader={isLoading} text="Развернуть" type="submit" disabled={isButtonDisabled} />
       </form>
       {circles.length > 0 && (
         <div className={styles.scheme}>
