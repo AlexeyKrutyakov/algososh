595cb808059bcbed836914ed8f497615d58a4c08
AlexeyKrutyakov <coder.arctik@gmail.com>
2023-12-28 17:04:01 +0300

    fix: add class Queue

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -13,6 +13,69 @@ import {
 } from '../../utils';
 import { removeFromHead } from '../../utils/manipulate-with-head';
 import { HEAD, TAIL } from '../../constants/element-captions';
+import { TQueue } from '../../types/queue';
+
+class Queue<T> {
+  private elements: T[] = [];
+  private head: number | null = null;
+  private tail: number | null = null;
+  private size: number = 0;
+
+  constructor(size: number) {
+    this.size = size;
+  }
+
+  enqueue = (item: T): void => {
+    if (this.head === null || this.tail === null) {
+      this.elements[0] = item;
+      this.head = 0;
+      this.tail = 0;
+    } else {
+      if ((this.head || this.tail) === this.getSize() - 1) {
+        return;
+      } else {
+        this.elements[this.tail + 1] = item;
+        this.tail++;
+      }
+    }
+  };
+
+  dequeue = (): void => {
+    if (this.head === null || this.tail === null) return;
+
+    if (this.head === this.tail) {
+      this.tail = null;
+    } else {
+      this.head++;
+    }
+    this.elements.shift();
+  };
+
+  clear = (): void => {
+    this.elements = [];
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  };
+
+  getHead = (): number | null => {
+    return this.head;
+  };
+
+  getTail = (): number | null => {
+    return this.tail;
+  };
+
+  getElements = (): T[] => {
+    return this.elements;
+  };
+
+  getSize = (): number => {
+    return this.size;
+  };
+}
+
+const queue = new Queue<string>(7);
 
 export const QueuePage: React.FC = () => {
   const [isAddingDisabled, setIsAddingDisabled] = useState(false);
@@ -164,7 +227,7 @@ export const QueuePage: React.FC = () => {
           extraClass={styles.input}
           isLimitText={true}
           maxLength={4}
-          onChange={event => setStr(event.currentTarget.value)}
+          onChange={(event) => setStr(event.currentTarget.value)}
           disabled={isInputDisabled}
         />
         <Button
