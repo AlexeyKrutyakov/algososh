a478ce619b3bc3f89525ea3f335b3cc4b275c141
Alexey Krutyakov <coder.arctik@gmail.com>
2024-01-27 20:40:51 +0300

    feat: add checks for testing sortingBySelection with index

--- src/utils/sort-by-selection.test.js
+++ src/utils/sort-by-selection.test.js
@@ -31,4 +31,21 @@ describe('Сортировка массива выбором', () => {
     const arr = [3, 1, 4, 2];
     expect(sortBySelection(arr, Direction.Descending)).toEqual([4, 3, 2, 1]);
   });
+
+  it('Сортировка по возрастанию с парамером index массива из нескольких элементов', () => {
+    const arr = [3, 1, 4, 2];
+    expect(sortBySelection(arr, Direction.Ascending, 0)).toEqual([1, 3, 4, 2]);
+  });
+
+  it('Сортировка по убыванию с параметром index массива из нескольких элементов', () => {
+    const arr = [3, 1, 4, 2];
+    expect(sortBySelection(arr, Direction.Descending, 1)).toEqual([3, 4, 1, 2]);
+  });
+
+  it('Сортировка по убыванию с параметром index = (arr.length - 1) массива из нескольких элементов', () => {
+    const arr = [3, 1, 4, 2];
+    expect(sortBySelection(arr, Direction.Descending, arr.length - 1)).toEqual([
+      3, 1, 4, 2,
+    ]);
+  });
 });
